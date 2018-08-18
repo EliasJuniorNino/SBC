05e1b4716de23ea33c2b986a296f812983a50726
EliasJuniorNino <eliasjunior_nino@hotmail.com>
2018-08-18 18:52:50 -0400

    izi tbem

--- 2017/Aquecimento/C/run.c
+++ 2017/Aquecimento/C/run.c
@@ -4,6 +4,12 @@
 #include <string.h>
 
 int main(int argc, char const *argv[]) {
-
-   return 0;
+    int C, A, cont = 0;
+    scanf("%d %d", &C, &A);
+    while(A > 0){
+        A -= (C - 1);
+        cont++;
+    }
+    printf("%d\n", cont);
+    return 0;
 }
